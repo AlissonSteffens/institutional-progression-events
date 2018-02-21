c4a3980fbaeb14735f511d0ca29755b773566466
AlissonSteffens <alisson.steffens@hotmail.com>
2018-02-21 14:02:36 -0300

    enviando coisas

--- app/routes/eventRouter.js
+++ app/routes/eventRouter.js
@@ -15,6 +15,11 @@ router.get('/:evento', async (req, res) => {
   result !== 'error' ? res.send(result) : res.send({'error': 'An error has occurred'})
 })
 
+router.get('/:evento/:usuario/:perfil', async (req, res) => {
+  res.type('text/html')
+  res.sendfile('public/chart.html')
+})
+
 router.delete('/:evento', async (req, res) => {
   const result = await asyncHandler.handleAsyncMethod(dbController.deleteSchema, [Event, req.params.evento])
   result !== 'error' ? res.send(`Event ${req.params.evento} deleted!`) : res.send({'error': 'An error has occurred'})
